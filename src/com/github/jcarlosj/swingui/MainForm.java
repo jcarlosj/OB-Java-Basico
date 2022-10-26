262b212be15d064f07297f13de621fcd519e6612
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-10-26 09:58:49 -0500

    [+] Swing UI: Establece tamaño, ubicacion en pantalla

--- src/com/github/jcarlosj/swingui/MainForm.java
+++ src/com/github/jcarlosj/swingui/MainForm.java
@@ -1,6 +1,7 @@
 package com.github.jcarlosj.swingui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainForm {
     private JLabel priceLabel;
@@ -13,8 +14,15 @@ public class MainForm {
     private JPanel panelForm;
 
     public static void main(String[] args) {
+        // Crea jframe como de costumbre
         JFrame frame = new JFrame( "Swing - Iva" );
         frame.setContentPane( new MainForm().panelForm );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        // Establece el tamaño, ubicación del jframe, y su visibilidad
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible( true );
     }
 }
